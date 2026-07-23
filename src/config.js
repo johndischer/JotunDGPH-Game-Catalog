387efccb8d503a260d94dd32bed2b499de12e1fc
johndischer <discher.johnroman@gmail.com>
2026-07-23 21:35:49 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const SITE_CONFIG = {
   // REQUIRED: paste your deployed Google Apps Script Web App URL ending in /exec.
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbyio9l-eaqFBeFTE3DlHN6FDaFPaDnPXDxnD9zdS8vmr6NsbpslpyRW6AgL682vSqo5/exec",
 
   messengerUrl: "https://m.me/JotunDGPH",
   waitlistUrl: "https://tally.so/r/XxQY4O",
